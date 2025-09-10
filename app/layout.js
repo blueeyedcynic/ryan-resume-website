c402a4a34d998c72162a3cbbec14fe424ab76f11
blueeyedcynic <42555491+blueeyedcynic@users.noreply.github.com>
2025-09-10 13:11:49 -0500

    added speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <title>Ryan Andrews - Product Management</title>
+      </head>
+      <body className={inter.className}>{children}
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
